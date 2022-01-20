b8f0e1e280a2cd59e654ce9ddc3c2d2f670cbb8d
Patrik Jarnesand <ouatis@gmail.com>
2022-01-20 15:22:26 +0100

    add eventlist and test case

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import EventList from './EventList'
 
 function App() {
   return (
     <div className="App">
-      
+      <EventList />
     </div>
   );
 }
