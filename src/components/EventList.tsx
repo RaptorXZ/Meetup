2b8140507451c2be3e86e0fac1c01d1125853ddd
inga0101 <ingaa134@gmail.com>
2022-01-21 10:48:29 +0100

    event interface and ul

--- src/components/EventList.tsx
+++ src/components/EventList.tsx
@@ -1,8 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { nanoid } from 'nanoid';
+import { Events } from '../models/Events'
+
+const data: Events[] = [
+    {
+        id: nanoid(),
+        eventName: 'Karaoke for coders',
+        interests: ['Coding', 'Singing'],
+        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut voluptas velit voluptates dolorum consequatur assumenda.',
+        location: 'Gothenburg',
+        date: '12/3/2022',
+        time: '18:00',
+        hostName: 'Daniel_1212'
+    },
+    {
+        id: nanoid(),
+        eventName: 'Karaoke for coders',
+        interests: ['Coding', 'Singing'],
+        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut voluptas velit voluptates dolorum consequatur assumenda.',
+        location: 'Gothenburg',
+        date: '12/3/2022',
+        time: '18:00',
+        hostName: 'Daniel_1212'
+    },
+    {
+        id: nanoid(),
+        eventName: 'Karaoke for coders',
+        interests: ['Coding', 'Singing'],
+        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut voluptas velit voluptates dolorum consequatur assumenda.',
+        location: 'Gothenburg',
+        date: '12/3/2022',
+        time: '18:00',
+        hostName: 'Daniel_1212'
+    },
+    {
+        id: nanoid(),
+        eventName: 'Karaoke for coders',
+        interests: ['Coding', 'Singing'],
+        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut voluptas velit voluptates dolorum consequatur assumenda.',
+        location: 'Gothenburg',
+        date: '12/3/2022',
+        time: '18:00',
+        hostName: 'Daniel_1212'
+    },
+    {
+        id: nanoid(),
+        eventName: 'Karaoke for coders',
+        interests: ['Coding', 'Singing'],
+        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut voluptas velit voluptates dolorum consequatur assumenda.',
+        location: 'Gothenburg',
+        date: '12/3/2022',
+        time: '18:00',
+        hostName: 'Daniel_1212'
+    }
+]
 
 function EventList() {
+    const [eventList, setEventList] = useState<Events[]>(data)
+
     return (
-        <div>List of Events</div>
+            <ul>
+                {eventList.map(event => (   
+                    <li key={event.id}>
+
+                        <h3>{event.eventName}</h3>
+                        <p>{event.date}</p>
+                        <p>{event.time}</p>
+                        <p>{event.hostName}</p>
+
+                        {event.interests.map(interest => (
+                            <p>{interest}</p>
+                        ))}
+
+                    </li>
+                ))}
+            </ul>
     )
 }
 
---
+++ src/models/Events.ts
@@ -0,0 +1,12 @@
+
+
+export interface Events {
+    id: string
+    eventName: string
+    hostName: string
+    location: string
+    date: string
+    time: string
+    description: string
+    interests:string[]
+}
