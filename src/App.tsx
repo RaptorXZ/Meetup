853e9d137a18f80b5f41a5cacb75914d9739008f
Johannakronqvist <78489287+Johannakronqvist@users.noreply.github.com>
2022-01-24 15:41:20 +0100

    added interests component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import EventList from './components/EventList'
+import Interests from './components/Interests';
 
 function App() {
   return (
     <div className="App">
       <EventList />
+	  <Interests />
     </div>
   );
 }
