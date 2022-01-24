619916b04b7f80fb20caf924f8adad517053861b
inga0101 <ingaa134@gmail.com>
2022-01-24 15:35:20 +0100

    fixed so event details only shows 1 event

--- src/components/EventDetails.tsx
+++ src/components/EventDetails.tsx
@@ -7,24 +7,33 @@ interface Props{
 }
 
 const EventPage = ({eventDetails, id}: Props) => {
-
+    console.log(eventDetails, id) // eventDetails, id
     // const { title } = useParams();
     // const profile = Data.filter(profile => profile.title === title)
-
+    
     // let updatedPlaylist = playlists.map((playlist) => {
-    //     if(playlist.id == id) {
-    //       playlist.ids = playlist.ids.filter((id) => (id.id != playlist.id))
-    //     }
-
-    const filterDetails = eventDetails.filter(filterDetails => filterDetails.id === id) // === id
-    console.log(filterDetails)
+        //     if(playlist.id == id) {
+            //       playlist.ids = playlist.ids.filter((id) => (id.id != playlist.id))
+            //     }
+            
+            const filterDetails = eventDetails.filter(filterDetails => filterDetails.id === id) // === id
+/*             console.log(filterDetails) */
+            console.log(id)
 
     return(
         <div role="eventDetails">
             {filterDetails.map(details => (
                 <section key={details.id}>
                     <h3>{details.eventName}</h3>
+                        <p>{details.date}</p>
+                        <p>{details.time}</p>
+                        <p>{details.location}</p>
+                        <p>{details.hostName}</p>
+                    <img src={details.image} alt={details.eventName} height="150px" />
                     <p>{details.description}</p>
+                    {details.interests.map(interest => (
+                            <p aria-labelledby="interest-label">{interest}</p>
+                        ))}
                 </section>
             ))}
         </div>
