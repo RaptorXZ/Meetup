b260394d9bb358a5da4aec0186fc3ea473c65c32
Patrik Jarnesand <ouatis@gmail.com>
2022-01-26 13:32:21 +0100

    merge from main

---
+++ src/components/CommentSection.test.tsx
@@ -0,0 +1,24 @@
+import { render, screen, queryByText, within } from '@testing-library/react'
+import userEvent from "@testing-library/user-event"
+import CommentSection from './CommentSection'
+import EventDetails from './EventDetails'
+import EventList from './EventList'
+
+describe('CommentSection component', () => {
+
+    it('adds a new comment when user inputs something into the comment field and presses enter', () => {
+        render(<EventList/>)
+        const [event] = screen.getAllByRole('listitem')
+        userEvent.click(event)
+
+        const [button] = screen.getAllByText('Attend')
+        userEvent.click(button)
+
+        // Get the comment input field
+        const input = screen.getByRole('textbox')
+        // Simulate keyboard event to input a comment
+        // Simulate enter
+        // Query an element with the text we inputted
+    })
+
+})
--- src/components/EventDetails.tsx
+++ src/components/EventDetails.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react"
 import { Events } from '../models/Events'
 import Commentsection from './CommentSection'
-
 import './EventDetails.css'
+
 interface Props{
     eventDetails: Events[]
     id: Events['id']
