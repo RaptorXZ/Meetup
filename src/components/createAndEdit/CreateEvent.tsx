30645f82102cc8d006c586849f6ddcf98e6d7f57
inga0101 <ingaa134@gmail.com>
2022-01-29 12:27:05 +0100

    added checkboxes for interests

--- src/components/createAndEdit/CreateEvent.tsx
+++ src/components/createAndEdit/CreateEvent.tsx
@@ -11,10 +11,11 @@ interface Props{
 const CreateEvent = ({events, addEvent}: Props) => {
     const [showForm, setShowForm] = useState(false)
     const [meetup, setMeetup] = useState(events)
+    const cehckInterest: string[] = ['Art', 'Food', 'Sports', 'Coding', 'Theatre', 'Movies', 'Gaming', 'Literature', 'Singing', 'Photography', 'Online', 'OnLocation', 'Tech', 'Music']
 
     const [eventName, setEventName] = useState('')
     const [image, setImage] = useState('')
-    const [interest, setInterest] = useState(['']) //add check boxes for interests
+    const [interest, setInterest] = useState<string[]>([]) //add check boxes for interests
     const [description, setDescription] = useState('')
     const [location, setLocation] = useState('')
     const [date, setDate] = useState('')
@@ -64,6 +65,34 @@ const CreateEvent = ({events, addEvent}: Props) => {
         }
     }
 
+    // Add/Remove checked interests
+    const handleCheck = (event: any) => {
+        let updatedList = [...interest]
+        if (event.target.checked) {
+        updatedList = [...interest, event.target.value]
+        } else {
+        updatedList.splice(interest.indexOf(event.target.value), 1)
+        }
+        setInterest(updatedList)
+    }
+
+
+    // const handleImageChange = function (e: React.ChangeEvent<HTMLInputElement>) {
+    //     const fileList = e.target.files
+    
+    //     if (!fileList) return;
+        
+    //     setImage(fileList[0]);
+    //   };
+
+    // const uploadFile = function (e: React.MouseEvent<HTMLSpanElement, MouseEvent>) {
+    //     if (image) {
+    //         const formData = new FormData();
+    //         formData.append("image", image, image.name)
+    //     }
+    // }
+    // handleImageChange uploadFile
+
 
     return(
         <div>
@@ -74,38 +103,46 @@ const CreateEvent = ({events, addEvent}: Props) => {
                 <form className="newEventForm">
                 <h3>create your new event!</h3>
 
-                <label htmlFor="mname">Meetup name:</label>
-                <input type="text" placeholder="give your meetup a name..." value={eventName} onChange={(e) => setEventName(e.target.value)}/>
-
-                <label htmlFor="description">Add description:</label>
-                <input type="text" placeholder="describe you meetup..." value={description} onChange={(e) => setDescription(e.target.value)}/>
-
-                <label htmlFor="location">Add location:</label>
-                <input type="text" placeholder="location..." value={location} onChange={(e) => setLocation(e.target.value)}/>
-
-                <label htmlFor="date">Add a date:</label>
-                <input type="text" placeholder="date..." value={date} onChange={(e) => setDate(e.target.value)}/>
-
-                <label htmlFor="time">Add a time:</label>
-                <input type="text" placeholder="time..." value={time} onChange={(e) => setTime(e.target.value)}/>
-
-                <label htmlFor="hname">Add your name:</label>
+                <label htmlFor="hname">Your name:
                 <input type="text" placeholder="describe you meetup" value={hostName} onChange={(e) => setHostName(e.target.value)}/>
+                </label>
 
-                <label htmlFor="image">Upload a photo:</label>
-                <input type="file" placeholder="upload a photo" value={image} onChange={(e) => setImage(e.target.value)}/>             
+                <label htmlFor="mname">Meetup name:
+                <input type="text" placeholder="give your meetup a name..." value={eventName} onChange={(e) => setEventName(e.target.value)}/>
+                </label>
+
+                <label htmlFor="description">Description:
+                <textarea placeholder="describe you meetup..." value={description} onChange={(e) => setDescription(e.target.value)}/>
+                </label>
+
+                <label htmlFor="location">Location:
+                <input type="text" placeholder="location..." value={location} onChange={(e) => setLocation(e.target.value)}/>
+                </label>
+
+                <label htmlFor="date">Date:
+                <input type="text" placeholder="date..." value={date} onChange={(e) => setDate(e.target.value)}/>
+                </label>
+
+                <label htmlFor="time">Time:
+                <input type="text" placeholder="time..." value={time} onChange={(e) => setTime(e.target.value)}/>
+                </label>
+
+                {cehckInterest.map((interest) => (
+                    <label htmlFor="interests">{interest}
+                    <input type="checkbox" value={interest} onChange={handleCheck}/>
+                    </label>
+                ))}
+
+                <label htmlFor="image">Upload a photo:
+                <input type="file" accept="image/*"  onChange={(e) => setImage(e.target.value)} />             
+                </label>
 
                 </form> 
 
-            <button onClick={() => submitMeetup()}>save meetup</button>
+                <button onClick={() => submitMeetup()}>save meetup</button>
 
             </div>)
             : null}
-
-
-
-
-
         </div>
     )
 }
